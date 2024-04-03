0d61e3db8cd735af8af3d4a0f701208825b38038
kyaloeriq <kyaloeriq@gmail.com>
2024-04-03 08:04:33 -0700

    function that goes through a binary tree using post-order traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through a binary tree post-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  */
@@ -9,7 +9,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
-	
+
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
 	(*func)(tree->n); /*Call the function on the current node*/
